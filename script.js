6e9cbf272eebee24a562f983fb2067387a482cbb
sagar kumar mathuri <sagarku2002@gmail.com>
2026-04-07 00:12:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,41 @@
-//complete this code
-class Rectangle {}
+// //complete this code
+// class Rectangle {}
 
-class Square extends Animal {}
+// class Square extends Animal {}
+
+// // Do not change the code below this line
+// window.Rectangle = Rectangle;
+// window.Square = Square;
+
+//complete this code
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return this.width * 4;
+  }
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
